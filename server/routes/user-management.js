7b0823ad4c36ad6a6b2f993afb7deee250c290a1
Prince Christian Basiga <princebasiga@gmail.com>
2018-08-21 22:51:32 -0700

    Implemented routes for resetting password of account

--- server/routes/user-management.js
+++ server/routes/user-management.js
@@ -5,7 +5,7 @@ const {emailFooter } = require("../../email-templates");
 const CES__HOME_URL = "http://localhost:3000/"
 
 const UIDGenerator = require('uid-generator');
-var userManager =  users.userManager();
+const uidGenerator = new UIDGenerator();
 
 
 //In previous version I didn't do verifying email, I'll do here though
@@ -40,6 +40,7 @@ app.post('/register',(req,res)=>{
           lastName : body.lastName,
           major : body.major,
           credits : 0,
+          verified: false,
        })
        .then(val => {
          console.log(val);
@@ -121,3 +122,153 @@ app.post("/send_account_verification", async (req,res) => {
  
 });
 
+
+
+//Forget password and token routes.
+
+
+
+//Might replace this with firestore.
+app.get("/check-token",(req,res)=>{
+
+    
+    const token = req.query.token;
+
+
+    //This checks if in expired
+    //Auto expired if is
+    //But also need to check if it's generated tokens
+    //Otherwise I'm saying it worked without confirming that token even ever existed.
+    
+    const dbRef = admin.database().ref("ExpiredTokens/"+token);
+    dbRef.once('value')
+      .then(snapshot => {
+        
+        if (snapshot.exists()){
+            res.send({expired:true});
+        }
+        else{
+
+          //Prob do this in firestore instead tbh, it's ust better lol.
+          //But fine for now, not big issue.
+          const tokensRef = admin.database().ref("Tokens/"+token);
+
+          tokensRef.once('value')
+            .then (snapshot => {
+              
+              //More like invalid not expired but ye.
+              if (!snapshot.exists()){
+                res.send({expired:true});
+                
+              }
+              else{
+
+                res.send({expired:false});
+              }
+
+            })
+
+        }
+      })
+      .catch(err => {
+
+        console.log(err);
+      })
+  
+})
+
+//Token expired when used as in link visited and or run out of time, because expired 
+//cause if expired then routes
+app.post("/token-used",(req,res)=>{
+
+    //This will remove the token, from tokens and push it into expired tokens.
+    const token = req.body.token;
+  
+    const dbRef = admin.database();
+  
+    //Don't want to remove here, cause honestly it's dumb, cause then would have to copy, just need this to dis-allow repeated access.
+    dbRef.ref("ExpiredTokens/"+token).push(token,val => {console.log(val);});
+  
+  })
+  
+  app.post("/confirm-reset",(req,res) => {
+  
+    
+      const body = req.body;
+  
+      const token = body.token;
+      const password = body.password;
+  
+      const dbRef = admin.database();
+      const tokensRef = dbRef.ref("Tokens/"+token);
+
+
+      tokensRef.once('value',snapshot => {
+  
+          const auth = admin.auth();
+  
+          var uid = null
+          snapshot.forEach(child => {
+
+                uid = child.val();
+          })
+  
+          auth.updateUser(uid,{
+            password:password
+          })
+            .then(user => {
+
+              dbRef.ref("ExpiredTokens/"+token).remove();
+              res.send({})
+              
+            })
+            .catch(err => {
+              console.log(err);
+            })
+            
+          //Regardlesss if succeeded or not this token is gone.          
+            tokensRef.remove();
+  
+      })
+  
+  })
+  app.post("/reset-password",(req,res)=>{
+    const auth = admin.auth();
+    const body = req.body;
+    
+    
+    //This can stay realtime, since no real organization to structure needed here.
+    auth.getUserByEmail(body.email)
+      .then(userRecord => {
+  
+
+        //Something to change, small chance. But still chance that this token is used
+        //BUt very low considering these will also expire.
+          uidGenerator.generate()
+            .then(token => {
+             
+              const dbRef = admin.database();
+              //Stores it alongside user.
+              dbRef.ref("Tokens/"+token).push(userRecord.uid);
+
+              const link = "'"+homeURL+"account/reset?resetToken="+token+"'";
+
+              const mailOptions = {
+                from: process.env.NOTIFIER_EMAIL,
+                to: body.email,
+                subject:"Password Reset Request",
+                html:"<p> Hello </p> <br> " + 
+                "<p> Someone has requested a password reset for the account associated with this email. \
+                If you did not make this request, ignore this email. </p> \
+                <br><br><a href="+link+"> Click here to reset your password</a> <br><br> <p> Best, <br> Cogswell Engineering Society</p>",
+              }
+
+              emailer.sendMail(mailOptions);
+              res.send({});
+            })
+      })
+      .catch(err => {
+        console.log(err);
+      })
+   
+  })
